47c36efa8a943762c28742866b2de377329d1db4
Virtuode <pathansehjad41@gmail.com>
2026-04-10 16:23:16 +0530

    Big Changes occur

---
+++ src/app/components/landing-page/landing-page.ts
@@ -0,0 +1,10 @@
+import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.css',
+})
+export class LandingPage {}
